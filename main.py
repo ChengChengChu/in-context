769b9a1ae63c7c4655ff2d8616e3fdb012fc294d
鄭承櫸 <chihuahuaiscute@zhengchengjudeMacBook-Pro.local>
2023-04-11 14:46:44 +0800

    fix_idx

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main():
     
     for sens in sentences :
         score, re_sen, re_res = bias_reward([sens], Bot, analyzer)
-        tmp = [score[0], re_sen[0], re_sen[1], re_res[0], re_res[1]]
+        tmp = [score[0], re_sen[0][0], re_sen[0][1], re_res[0][0], re_res[0][1]]
         result.append(tmp)
         o += 1
         if o >= 20 : break
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python3 main.py \
 --prompt_path prompts/chat_key_500.csv \
---save_path results \
---exp_name chatgpt_keywords \
+--save_path dialogGPT_random_key.csv \
 --bot DialogGPT 
