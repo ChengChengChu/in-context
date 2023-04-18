2c3a68ee49e413bc2cb593ef543f5b13ba717a95
ChengChengChu <85456619+ChengChengChu@users.noreply.github.com>
2023-04-18 16:14:28 +0800

    Update module.py

--- bots/module.py
+++ bots/module.py
@@ -3,8 +3,7 @@ import re
 
 class agent() :
     def __init__(self) :
-        openai.organization = "org-9VP7zbu5OprKdttIEI0m2wqX"
-        openai.api_key = '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'
+        key + organ
         
         
         self.template = "Please read the following conversation about Speaker A and Speaker B. The goal of this task is to rate words Speaker B spoke.\nConversation :\n [CONV]\n(End of conversation fragment)\nHow comforting is Speaker B's words?(on a scale of 1-5, with 1 being the lowest). You cannot say nothing about this conversation."
